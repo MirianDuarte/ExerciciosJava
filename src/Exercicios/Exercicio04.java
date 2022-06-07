3fefc07d557aa8a988fa56f5046be3cfaf3517e9
MirianDuarte <contatomirianduarte@hotmail.com>
2022-06-06 21:24:51 -0300

    Exercicio04

---
+++ src/Exercicios/Exercicio04.java
@@ -0,0 +1,35 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c;
+		double R, S, D, baseR, baseS;
+		
+		System.out.println("Digito valor de A: ");
+		a = sc.nextInt();
+		
+		System.out.println("Digite o valor de B: ");
+		b = sc.nextInt();
+		
+		 System.out.println("Digite o valor de C: ");
+		 c = sc.nextInt();
+		 
+		 baseR = a + b;
+		 R = Math.pow(baseR, 2);
+		 baseS = b + c;
+		 S = Math.pow(baseS, 2);
+		 
+		 D = (R + S) / 2;
+		 
+		 System.out.println(D);
+		 
+		 sc.close();
+	}
+
+}
