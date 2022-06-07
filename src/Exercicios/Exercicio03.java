2c077fe1abb50175caa7b03215e93a5075596e06
MirianDuarte <contatomirianduarte@hotmail.com>
2022-06-06 21:05:56 -0300

    Exercicio 03

---
+++ src/Exercicios/Exercicio03.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o tempo em segundos: ");
+		int tempo = sc.nextInt();
+		
+		int horas = tempo / 3600;
+		int resto = tempo % 3600;
+		
+		int minutos = resto / 60;
+		int segundos= resto % 60;
+		
+		System.out.printf("O evento durou %d horas, %d minutos e %d segundos." , horas, minutos, segundos);
+		
+		sc.close();
+	
+	}
+
+}
